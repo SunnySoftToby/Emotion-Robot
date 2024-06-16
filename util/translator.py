c1325d1c57ed95950b9faf698e17464cb38dda3a
SunnySoft <yangme9@gmail.com>
2024-06-17 01:26:44 +0800

    新增實驗和分析

--- util/translator.py
+++ util/translator.py
@@ -14,18 +14,18 @@ class Translator:
         self.tokenizer.padding_side = 'left'
         self.model = AutoModelForCausalLM.from_pretrained("./model_weight/TAIDE-LX-7B-Chat", torch_dtype=torch.float16)
         self.model.to(self.device)
+        self.sys = "將英文句子翻譯成繁體中文，返回翻譯結果就可以，不要進行任何解釋"
 
     def translate(self, message):
-        sys = "將英文句子翻譯成繁體中文，返回翻譯結果就可以，不要進行任何解釋"
-        prompt = f"<s>[INST] <<SYS>>\n{sys}\n<</SYS>>\n\n{message} [/INST]</s>"
+        prompt = f"<s>[INST] <<SYS>>\n{self.sys}\n<</SYS>>\n\n{message} [/INST]</s>"
         self.tokenizer.padding_side = 'left'
 
-        inputs = self.tokenizer(prompt, return_tensors="pt", padding='max_length', truncation=True, max_length=512).to(
+        inputs = self.tokenizer(prompt, return_tensors="pt", padding='max_length', truncation=True, max_length=1024).to(
             self.device)
-        output = self.model.generate(**inputs, max_new_tokens=512, pad_token_id=self.tokenizer.pad_token_id)
+        output = self.model.generate(**inputs, max_new_tokens=1024, pad_token_id=self.tokenizer.pad_token_id)
         full_response = self.tokenizer.decode(output[0], skip_special_tokens=True)
 
-        response_start = full_response.find("[/INST]") + len("[/INST]")
+        response_start = full_response.rfind("[/INST]") + len("[/INST]")
         translation = full_response[response_start:].strip()
 
         # clean cache to avoid out-of-memory
