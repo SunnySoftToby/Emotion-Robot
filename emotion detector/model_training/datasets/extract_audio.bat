47f3d3599e8bf49a16e63fa5542f4a3158d38a99
chenp6 <52535982+chenp6@users.noreply.github.com>
2024-06-21 16:02:01 +0800

    Update extract_audio.bat

--- emotion detector/model_training/datasets/extract_audio.bat
+++ emotion detector/model_training/datasets/extract_audio.bat
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 for /r "<影片資料夾位址>" %%f in (*.mp4) do (
     REM 獲得wav與資料夾編號
     for %%I in ("%%~pf.") do (
-        set "output_folder=D:\CMU-MultimodalSDK\project\raw_dataset\Raw\audios\%%~nxI"
+        set "output_folder=<圖片輸出資料夾位址>\audios\%%~nxI"
        
         echo output_folder
 
@@ -14,9 +14,7 @@ for /r "<影片資料夾位址>" %%f in (*.mp4) do (
         )
 
         rem only use left side
-        "D:\ffmpeg-7.0-essentials_build\ffmpeg-7.0-essentials_build\bin\ffmpeg.exe" -i "%%f" -ar 16000 -filter_complex "channelsplit=channel_layout=stereo:channels=FL[left]" -map "[left]" -t 10 "!output_folder!\%%~nxI_%%~nf.wav"
+        "ffmpeg -i "%%f" -ar 16000 -filter_complex "channelsplit=channel_layout=stereo:channels=FL[left]" -map "[left]" -t 10 "!output_folder!\%%~nxI_%%~nf.wav"
         
-        rem right side channel
-        rem ffmpeg -i "%%f" -ar 16000 -filter_complex "channelsplit=channel_layout=stereo:channels=FR[right]" -map "[right]" "!output_folder!\%%~nxI_%%~nf_right.wav"
     )
 )
